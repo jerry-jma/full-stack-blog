32cebf12665ae001f04a82e3b2738c16952b0fd5
junma <junma0704@gmail.com>
2022-12-18 23:35:05 -0800

    feat: Afit single, write and home pages inside the nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet
 } from "react-router-dom";
 
 import Register from './pages/Register'
@@ -12,15 +13,34 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 function App() {
+  const Layout = () => {
+    return (
+      <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+      </>
+    )
+  }
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>
-        <Navbar />
-        <Home />
-        <Footer />
-      </div>,
+     element: <Layout />,
+     children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/single",
+        element: <Single />
+      },
+      {
+        path: "/write",
+        element: <Write />
+      }
+     ]
     },
     {
       path: "/register",
