2643f155b09053095652246c8835da1271db18c1
junma <junma0704@gmail.com>
2022-12-21 14:55:10 -0800

    feat: hash the auth password

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -1,4 +1,26 @@
-export const register = (req, res) => {};
+import { db } from "../db.js";
+import bcrypt from "bcrypt";
+
+export const register = (req, res) => {
+  // Check existing users
+  const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+
+  db.query(q, [req.body.email, req.body.username], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length) return res.status(409).json("User already exists!");
+
+    //Hash the password and create a user
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const q = "INSERT INTO users(`username`, `email`, `password`) VALUES(?)";
+
+    db.query(q, [req.body.username, req.body.email, hash], (err, data) => {
+      if (err) return res.json(err);
+      return res.status(200).json("User has been created");
+    });
+  });
+};
 
 export const login = (req, res) => {};
 
