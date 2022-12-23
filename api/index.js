e159bd52a716e8e2e1413bc98e8f0794c7194695
junma <junma0704@gmail.com>
2022-12-23 10:53:17 -0800

    feat: Use multer to handle image upload

--- api/index.js
+++ api/index.js
@@ -3,12 +3,18 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
+const upload = multer({ dest: "./uploads/" });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("Image has been uploaded");
+});
 // we could just use app.get() to call the exact endpoint, but with many same endpoints, the file can become messy. Also, with Router and controller folder, we seperate these complexity out to speific file, it is easlier to manage
 
 app.use("/api/auth", authRoutes);
