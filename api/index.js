d5718a0be0c62e9f454b52e269250e25f7a5783e
junma <junma0704@gmail.com>
2022-12-23 11:08:52 -0800

    feat: upload images with its extension using storage

--- api/index.js
+++ api/index.js
@@ -10,10 +10,23 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const upload = multer({ dest: "./uploads/" });
+// const upload = multer({ dest: "./uploads/" });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/uploads");
+  },
+  filename: function (req, file, cb) {
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, Date.now() + "_" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("Image has been uploaded");
+  const file = req.file;
+  res.status(200).json(file.filename);
 });
 // we could just use app.get() to call the exact endpoint, but with many same endpoints, the file can become messy. Also, with Router and controller folder, we seperate these complexity out to speific file, it is easlier to manage
 
